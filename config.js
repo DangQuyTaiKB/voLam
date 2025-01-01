0dfbd26445be644928d5bff57cd23670de6cc32e
DangQuyTaiKB <dangquytai776617185@gmail.com>
2025-01-01 22:00:06 +0100

    Nice

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à❤️!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu anh❤️',
+    btnNo: 'Yêu anh part time🙂',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh❤️',
+    reply: 'Yêu thì yêu mà không yêu thì yêu❤️❤️❤️',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều😘😘',
+    messDesc: 'Thấy tình cảm người ta rồi thì cười lên nhé❤️❤️❤️',
+    btnAccept: 'Dạ❤️',
+    messLink: 'https://www.facebook.com/messages/e2ee/t/7086109494833461' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
